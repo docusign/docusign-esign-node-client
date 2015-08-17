84bdce429a3b7637ede612ffe09b05b6dc918340
Ergin Dervisoglu <ergin.dervisoglu@docusign.com>
2015-08-17 11:59:39 -0700

    fix bracket formatting

--- components/envelopes.js
+++ components/envelopes.js
@@ -673,13 +673,11 @@ function getSignedDocuments (apiToken, baseUrl, envelopeId, encoding, attachCert
 function getSignerView (apiToken, baseUrl, userId, recipientName, email, clientUserId, envelopeId, returnUrl, authMethod, callback) {
   var data = {};
   
-  if (clientUserId === null)
-  {
+  if (clientUserId === null) {
     return callback(new DocuSignError('clientUserId is a required recipient parameter for embedded signing.'));
   }
   
-  if (!userId)
-  {
+  if (!userId) {
     data = {
       returnUrl: returnUrl,
       authenticationMethod: authMethod,
