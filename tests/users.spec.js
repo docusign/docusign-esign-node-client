f72279785ab6c4de1b44163a77f1446fe8450bc1
C.J. Winslow <whoaa512@gmail.com>
2015-10-15 15:43:46 -0700

    port over users tests

---
+++ tests/users.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const test = require('ava');
+const docusign = require('../docusign');
+const config = require('./test-config.json');
+
+let client = null;
+let userId = null;
+
+test.before(function before (t) {
+  return docusign.init(config.integratorKey, config.apiEnv, config.debug)
+  .then(response => {
+    t.ok(response.message === 'successfully initialized');
+    return docusign.createClient(config.email, config.password);
+  })
+  .then(_client => {
+    client = _client;
+    return client.admin.getUserList();
+  })
+  .then(userList => {
+    userId = userList[0].userId;
+  });
+});
+
+test.after(function after (t) {
+  return client.logOut();
+});
+
+test(function getInfo (t) {
+  return client.users.getInfo(userId)
+  .then(response => {
+    t.ok(response.userName);
+    t.same(response.userId, userId);
+  });
+});
+
+test(function getSocialConnection (t) {
+  return client.users.getSocialConnection(userId)
+  .then(response => {
+    t.ok(response.socialAccountInformation);
+    t.same(response.userId, userId);
+  });
+});
