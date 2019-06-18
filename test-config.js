60cd1dd0a551422088341dcda75d6933f630e40f
Alex <alex.cooper@docusign.com>
2019-06-18 16:20:06 -0700

    config

---
+++ test-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  integratorKey: process.env.INTEGRATOR_KEY,
+  apiEnv: 'demo',
+  debug: false,
+  email: 'node_sdk@mailinator.com',
+  noSigEmail: 'no_sig@mailinator.com',
+  userId: process.env.USER_ID,
+  noSigUserId: process.env.NO_SIG_USER,
+  templateId: process.env.TEMPLATE_ID,
+  templateRole: 'bob',
+  integratorKeyAuthCode: process.env.INTEGRATOR_KEY_AUTH_CODE,
+  integratorKeyImplicit: process.env.INTEGRATOR_KEY_IMPLICIT,
+  clientSecret: process.env.CLIENT_SECRET
+};
