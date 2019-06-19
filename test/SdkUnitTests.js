64fa026fa57325ce3ed94641d0b5fa0f3e439e85
Alex <alex.cooper@docusign.com>
2019-06-19 01:13:40 -0700

    qa

--- test/SdkUnitTests.js
+++ test/SdkUnitTests.js
@@ -3,7 +3,7 @@ var oAuth = docusign.ApiClient.OAuth;
 var restApi = docusign.ApiClient.RestApi;
 var config;
 try {
-  config = require('../test-config.json');
+  config = require('../test-config');
 } catch (err) {
   console.error(err);
 }
