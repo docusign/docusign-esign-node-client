4544bd5b3aaba0ec2e0bb63c3cc43f333ecc2f89
Alex <alex.cooper@docusign.com>
2019-08-27 19:00:12 -0700

    credentials

---
+++ test-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  integratorKey: process.env.INTEGRATOR_KEY_JWT,
+  apiEnv: 'demo',
+  debug: false,
+  email: 'node_sdk@mailinator.com',
+  userId: process.env.USER_ID,
+  templateId: process.env.TEMPLATE_ID,
+  templateRole: 'bob',
+  integratorKeyAuthCode: process.env.INTEGRATOR_KEY_AUTH_CODE,
+  integratorKeyImplicit: process.env.INTEGRATOR_KEY_IMPLICIT,
+  clientSecret: process.env.CLIENT_SECRET,
+  brandId: process.env.BRAND_ID,
+  privateKey: process.env.PRIVATE_KEY
+};
