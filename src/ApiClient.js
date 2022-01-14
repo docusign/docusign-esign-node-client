c9cdff95e9463e3d5c5f1e1384fa2519a0f13f0e
Nima <nima.poulad@docusign.com>
2022-01-14 12:04:02 -0800

    Record the node version number

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -154,7 +154,7 @@
      * @type {Array.<String>}
      * @default {}
      */
-    this.defaultHeaders = { "X-DocuSign-SDK": "Node", "Node-Ver": process.version };
+    this.defaultHeaders = { "X-DocuSign-SDK": `Node - ${process.version}`, "Node-Ver": process.version };
 
     /**
      * The default HTTP timeout for all API calls.
