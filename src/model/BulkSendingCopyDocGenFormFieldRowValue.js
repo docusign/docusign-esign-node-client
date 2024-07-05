429c5226482cec3bbd74c83452ccba91874eb19f
Mudit Garg <35302867+garg-mudit@users.noreply.github.com>
2024-07-05 17:39:50 +0530

    Version 8.0.0-rc1-v2.1-24.2.00.00 release (#358)
    Co-authored-by: root <root@devcenteradmin.docusigntest.com>

--- src/model/BulkSendingCopyDocGenFormFieldRowValue.js
+++ src/model/BulkSendingCopyDocGenFormFieldRowValue.js
@@ -12,29 +12,29 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/DocGenFormField'], factory);
+    define(['ApiClient', 'model/BulksendingCopyDocGenFormField'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./DocGenFormField'));
+    module.exports = factory(require('../ApiClient'), require('./BulksendingCopyDocGenFormField'));
   } else {
     // Browser globals (root is window)
     if (!root.Docusign) {
       root.Docusign = {};
     }
-    root.Docusign.DocGenFormFieldRowValue = factory(root.Docusign.ApiClient, root.Docusign.DocGenFormField);
+    root.Docusign.BulkSendingCopyDocGenFormFieldRowValue = factory(root.Docusign.ApiClient, root.Docusign.BulksendingCopyDocGenFormField);
   }
-}(this, function(ApiClient, DocGenFormField) {
+}(this, function(ApiClient, BulksendingCopyDocGenFormField) {
   'use strict';
 
 
   /**
-   * The DocGenFormFieldRowValue model module.
-   * @module model/DocGenFormFieldRowValue
+   * The BulkSendingCopyDocGenFormFieldRowValue model module.
+   * @module model/BulkSendingCopyDocGenFormFieldRowValue
    */
 
   /**
-   * Constructs a new <code>DocGenFormFieldRowValue</code>.
-   * @alias module:model/DocGenFormFieldRowValue
+   * Constructs a new <code>BulkSendingCopyDocGenFormFieldRowValue</code>.
+   * @alias module:model/BulkSendingCopyDocGenFormFieldRowValue
    * @class
    */
   var exports = function() {
@@ -44,18 +44,18 @@
   };
 
   /**
-   * Constructs a <code>DocGenFormFieldRowValue</code> from a plain JavaScript object, optionally creating a new instance.
+   * Constructs a <code>BulkSendingCopyDocGenFormFieldRowValue</code> from a plain JavaScript object, optionally creating a new instance.
    * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
    * @param {Object} data The plain JavaScript object bearing properties of interest.
-   * @param {module:model/DocGenFormFieldRowValue} obj Optional instance to populate.
-   * @return {module:model/DocGenFormFieldRowValue} The populated <code>DocGenFormFieldRowValue</code> instance.
+   * @param {module:model/BulkSendingCopyDocGenFormFieldRowValue} obj Optional instance to populate.
+   * @return {module:model/BulkSendingCopyDocGenFormFieldRowValue} The populated <code>BulkSendingCopyDocGenFormFieldRowValue</code> instance.
    */
   exports.constructFromObject = function(data, obj) {
     if (data) {
       obj = obj || new exports();
 
       if (data.hasOwnProperty('docGenFormFieldList')) {
-        obj['docGenFormFieldList'] = ApiClient.convertToType(data['docGenFormFieldList'], [DocGenFormField]);
+        obj['docGenFormFieldList'] = ApiClient.convertToType(data['docGenFormFieldList'], [BulksendingCopyDocGenFormField]);
       }
     }
     return obj;
@@ -63,7 +63,7 @@
 
   /**
    * 
-   * @member {Array.<module:model/DocGenFormField>} docGenFormFieldList
+   * @member {Array.<module:model/BulksendingCopyDocGenFormField>} docGenFormFieldList
    */
   exports.prototype['docGenFormFieldList'] = undefined;
 
